5b1dc86f8fd7bbea2555db22c632a11769abb7f9
AkpoFlash <akpoflash@gmail.com>
2016-02-22 22:44:59 +0300

    Add popup title and fast top arrow

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 var curPosition = 50;
+var curSlide = 2;
+var hoverHeader = false;
+var topShow = 800;
+
+function changeTitle(){
+	switch(curPosition){
+		case 0:   curSlide = 1; break;
+		case 50:  curSlide = 2; break;
+		case 100: curSlide = 3; break;
+	}
+	$(".text-1").css({"opacity":"0"});
+	$(".text-2").css({"opacity":"0"});
+	$(".text-3").css({"opacity":"0"});
+	$(".text-"+curSlide).css({"opacity":"1"});
+}
 
 $(document).ready(function(){
 	$(".left-arrow").on("click",function(){
@@ -9,6 +24,7 @@ $(document).ready(function(){
 			curPosition -= 50;
 		}
 		$("header").css({"backgroundPosition":curPosition+"%"});
+		changeTitle();
 	});
 	$(".right-arrow").on("click",function(){
 		if(curPosition == 100){
@@ -18,5 +34,20 @@ $(document).ready(function(){
 			curPosition += 50;
 		}
 		$("header").css({"backgroundPosition":curPosition+"%"});
+		changeTitle();
+	});
+
+	$("#up").on("click", function(e){
+		$("html, body").animate({scrollTop: 0}, 1000);
+		e.preventDefault();
+	});
+
+	$(window).on("scroll", function(){
+		if($(this).scrollTop() > topShow){
+			$("#up").css({"transform": "translateX(0)"});
+		}
+		else{
+			$("#up").css({"transform": "translateX(100px)"});
+		}
 	});
 });
